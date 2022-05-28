a30a0dfc7d67bea72813a6e245998d749c7dbed7
FAinShop <fashop127@gmail.com>
2022-05-28 14:29:12 +0600

    feat: added quiz

--- 26-05/script.js
+++ 26-05/script.js
@@ -1,4 +1,4 @@
-const whois = prompt('Кто пришел?','Админ');
+/*const whois = prompt('Кто пришел?','Админ');
 
 if (whois === 'Админ'){
     const password = prompt('Пароль');
@@ -14,4 +14,23 @@ if (whois === 'Админ'){
     alert('Вход отменен');
 } else {
     alert('Я вас не знаю');
+}*/
+
+// 1
+let point = 0;
+let answer = '';
+answer = prompt('Столица Кыргызстана?');
+if(answer === 'Бишкек'){
+    point++;
+}else{
+    alert('Неверно');
 }
+
+answer = prompt('Самая большая страна по площади?');
+if (answer === 'Россия'){
+    point++;
+}else{
+    alert('Неверно');
+}
+
+alert('Вы набрали: ' + point + ' балл(a)');
